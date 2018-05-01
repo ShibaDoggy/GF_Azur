5c26387102557099e57aa5527f22a87123ab6244
YoRHa428 <vini0428@gmail.com>
2018-05-01 14:57:33 +0900

    reset

--- src/main/java/BotToken.java
+++ src/main/java/BotToken.java
@@ -1,29 +1,3 @@
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class BotToken {
-	
-	static String tokenUrl = null;
-	PreparedStatement statement = null;
-	ResultSet rs = null;
-
-	public Connection connection(String dbFileName) throws ClassNotFoundException, SQLException {
-		Class.forName("org.sqlite.JDBC");
-		String url = "jdbc:sqlite:database/idDB.db";
-		Connection conn = DriverManager.getConnection(url);
-		
-		
-		statement = conn.prepareStatement("select * from Code where ID = 'GRUPPE';");
-		rs = statement.executeQuery();
-		
-		tokenUrl = rs.getString("String");
-		
-		return conn;
-	}
-	
-	public final static String discordToken = tokenUrl;
+	public final static String discordToken = "NDMzNjk2NDI5NTA5MzEyNTIy.DcmQzQ.vZG8mcqNDk7XJv_7G9tLGGqRLCU";
 }
